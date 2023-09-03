7236d333661791bf596268d4b42a6a35a52a20aa
chiragziniya <chiragziniya@gmail.com>
2023-09-03 11:26:51 +0000

    theme added

--- api/index.py
+++ api/index.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+  return render_template('index.html')
+
 
 @app.route('/about')
 def about():
-    return 'About'
+  return 'About'
+
+
+@app.route('/blog')
+def blog():
+  return render_template('blog.html')
+
+
+@app.route('/blog-detail')
+def blog_detail():
+  return render_template('blog-details.html')
+
+
+@app.route('/portfolio-detail')
+def portfolio_details():
+  return render_template('portfolio-details.html')
+
+
+@app.route('/services-detail')
+def services_details():
+  return render_template('services-details.html')
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
